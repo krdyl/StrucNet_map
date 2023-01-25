b9deecd0f060e72332789f09ea5b5d6b801e2f1d
wcherlet <wout.cherlet@ugent.be>
2023-01-25 16:41:46 +0100

    initial commit - global TLS site

---
+++ google-doc-url.js
@@ -0,0 +1,6 @@
+// Google Sheets URL
+var googleDocMetadataURL = 'https://docs.google.com/spreadsheets/d/1d5BlpZQ3l2tTOqfxvRQB_TvKZdPM8x2qjjqTn0gRz7g/edit#gid=0';
+var googleDocConfigURL = 'https://docs.google.com/spreadsheets/d/1CdEG2zkD8NaZ6b2su85-_FpKemJs_eoS0fKFEB49SKk/edit#gid=1284643617';
+
+// Google Sheets API key
+var googleApiKey = 'AIzaSyDanPnCLHaibRMiGUbOERi40ElVTsMPhZY';
